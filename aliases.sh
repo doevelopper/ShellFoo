f6fda8148c2eca984ea1480dcec006885ccebd79
mpsyco <m_psyco2@hotmail.com>
2013-03-19 18:26:20 -0300

    Update aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -1,4 +1,5 @@
 
+# Be laaazzzyyy
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
@@ -10,9 +11,25 @@ alias ls='ls --color=auto'
 alias la="ls -a"
 alias lh="ls -lh"
 
+# Be safe
 alias cp='cp -i'
 alias mv='mv -i'
-alias rm='rm -i'
+alias ln='ln -i'
+alias rm='rm -i --preserve-root'
+
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
+
+# Be more human
+alias df='df -H'
+alias du='du -ch'
+
+
+alias mkdir='mkdir -pv'
+
+# Show open ports
+alias ports='netstat -tulanp'
 
 [ -f ${SHELLFOO_DIR}git.sh ] && . ${SHELLFOO_DIR}git.sh
 
